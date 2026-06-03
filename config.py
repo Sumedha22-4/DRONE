c4791faad62694a6e5022aeb9d8e0e21f9d88bf9
Sumedha22-4 <klehjehmeh@gmail.com>
2026-06-04 02:53:38 +0530

    Create config.py with email and bot settings
    Added configuration settings for Rescue Drone.

---
+++ config.py
@@ -0,0 +1,15 @@
+# Rescue Drone — laptop config (keep private; do not commit to git)
+
+# ── Put your XIAO ESP32S3 IP address here ────────────────────────────────────
+XIAO_IP = "192.168.43.65"   # <-- change this to the IP shown in Serial Monitor
+# ─────────────────────────────────────────────────────────────────────────────
+
+FROM_EMAIL   = "Sumedhamukherjee2004@gmail.com"
+FROM_NAME    = "Rescue Drone"
+TO_EMAIL     = "rakeshsoni48128@gmail.com"
+APP_PASSWORD = "zgrqyyyhcxkdnbce"
+
+TG_BOT_TOKEN = "8853836062:AAE28xC3qrGy2ymCiLXY8vf4Dh54HqDspeI"
+TG_CHAT_ID   = "7999773900"
+
+# Run:  python rescue_drone.pyp
